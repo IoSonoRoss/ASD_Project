c52f8f04fa2a9be03b6805890d25b226883cfadb
IoSonoRoss <m.rossini026@studenti.unibs.it>
2025-08-26 09:48:24 +0200

    Sistemato errore

--- path_logic.py
+++ path_logic.py
@@ -20,7 +20,7 @@ def is_path_free(grid: Grid, path, forbidden_obstacles=frozenset()):
             
     return True
 
-def genera_coordinate_percorso(origine, destinazione, diagonale_prima):
+def generate_path_coordinates(origin, destination, diagonal_first):
     """
     Genera una lista di coordinate che rappresentano un percorso dalla cella origine alla destinazione su una griglia.
     Il percorso consiste in mosse diagonali e rettilinee (orizzontali/verticali), con la possibilità di dare priorità alle mosse diagonali.
@@ -31,46 +31,44 @@ def genera_coordinate_percorso(origine, destinazione, diagonale_prima):
     Returns:
         list[tuple[int, int]]: Una lista di coordinate che rappresentano il percorso da origine a destinazione, inclusi gli estremi.
     """
-    percorso = [origine]
-    r_corr, c_corr = origine
-    r_dest, c_dest = destinazione
-    delta_r, delta_c = r_dest - r_corr, c_dest - c_corr
+    path = [origin]
+    r_curr, c_curr = origin
+    r_dest, c_dest = destination
+    delta_r, delta_c = r_dest - r_curr, c_dest - c_curr
     num_diag = min(abs(delta_r), abs(delta_c))
     num_rect = abs(abs(delta_r) - abs(delta_c))
-    passo_r, passo_c = int(np.sign(delta_r)), int(np.sign(delta_c))
+    step_r, step_c = int(np.sign(delta_r)), int(np.sign(delta_c))
 
     if abs(delta_r) > abs(delta_c):
-        passo_rect_r, passo_rect_c = passo_r, 0
+        rect_step_r, rect_step_c = step_r, 0
     else:
-        passo_rect_r, passo_rect_c = 0, passo_c
+        rect_step_r, rect_step_c = 0, step_c
 
-    def mosse_diagonali():
+    def make_diag_moves():
         """
         Genera le mosse in diagonale.
         """
-        nonlocal r_corr, c_corr
+        nonlocal r_curr, c_curr
         for _ in range(num_diag):
-            r_corr += passo_r
-            c_corr += passo_c
-            percorso.append((r_corr, c_corr))
+            r_curr += step_r; c_curr += step_c
+            path.append((r_curr, c_curr))
 
-    def mosse_rettilinee():
+    def make_rect_moves():
         """
         Genera le mosse rettilinee.
         """
-        nonlocal r_corr, c_corr
+        nonlocal r_curr, c_curr
         for _ in range(num_rect):
-            r_corr += passo_rect_r
-            c_corr += passo_rect_c
-            percorso.append((r_corr, c_corr))
+            r_curr += rect_step_r; c_curr += rect_step_c
+            path.append((r_curr, c_curr))
 
-    if diagonale_prima:
-        mosse_diagonali()
-        mosse_rettilinee()
+    if diagonal_first:
+        make_diag_moves()
+        make_rect_moves()
     else:
-        mosse_rettilinee()
-        mosse_diagonali()
-    return percorso
+        make_rect_moves()
+        make_diag_moves()
+    return path
 
 
 def calcola_distanza_libera(origin, destination):
